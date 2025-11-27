4560123c05f9f10bb2dd2a14e705c32367325228
Juri <juri.strumpflohner@gmail.com>
2025-11-27 15:47:45 +0100

    chore: move frontend packageds into subfolder

---
+++ apps/shop-e2e/eslint.config.mjs
@@ -0,0 +1,13 @@
+import playwright from 'eslint-plugin-playwright';
+import baseConfig from '../../eslint.config.mjs';
+
+export default [
+  { ignores: ['out-tsc/**'] },
+  playwright.configs['flat/recommended'],
+  ...baseConfig,
+  {
+    files: ['**/*.ts', '**/*.js'],
+    // Override or add rules here
+    rules: {},
+  },
+];
