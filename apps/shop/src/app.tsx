12b4c3a9968efee68a93879f87845dce2e191067
Juri <juri.strumpflohner@gmail.com>
2026-01-19 17:54:23 +0100

    feat: US-011 - Integrate auth in app

--- apps/shop/src/app.tsx
+++ apps/shop/src/app.tsx
@@ -7,23 +7,63 @@ import OrdersPage from './pages/OrdersPage';
 import PastOrdersPage from './pages/PastOrdersPage';
 import CreateOrderPage from './pages/CreateOrderPage';
 import { ProductDetailPage } from '@tusky/feat-product-detail';
+import { AuthProvider, ProtectedRoute } from '@tusky/util-auth';
+import { LoginPage } from '@tusky/feat-login';
 
 export function App() {
   return (
     <Router>
-      <div className="min-h-screen flex flex-col bg-gray-50">
-        <Navbar />
-        <main className="grow container mx-auto px-4 py-8">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/product/:id" element={<ProductDetailPage />} />
-            <Route path="/orders" element={<OrdersPage />} />
-            <Route path="/orders/past" element={<PastOrdersPage />} />
-            <Route path="/orders/create" element={<CreateOrderPage />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <AuthProvider>
+        <div className="min-h-screen flex flex-col bg-gray-50">
+          <Navbar />
+          <main className="grow container mx-auto px-4 py-8">
+            <Routes>
+              <Route path="/login" element={<LoginPage />} />
+              <Route
+                path="/"
+                element={
+                  <ProtectedRoute>
+                    <HomePage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/product/:id"
+                element={
+                  <ProtectedRoute>
+                    <ProductDetailPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/orders"
+                element={
+                  <ProtectedRoute>
+                    <OrdersPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/orders/past"
+                element={
+                  <ProtectedRoute>
+                    <PastOrdersPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/orders/create"
+                element={
+                  <ProtectedRoute>
+                    <CreateOrderPage />
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </AuthProvider>
     </Router>
   );
 }
